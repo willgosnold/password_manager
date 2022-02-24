2e738540768b2629b1dd13e9f4a7869a818139d2
Will Gosnold <willgosnold@googlemail.com>
2022-02-24 17:33:33 +0000

    Added various file altering functions

--- no_db.py
+++ no_db.py
@@ -1,13 +1,13 @@
 import base64
+from getpass import getpass
+import pandas as pd
 
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-from getpass import getpass
 from context import AccessEncrypted
 
-import pandas as pd
 
 salt = b"qwerty"
 kdf = PBKDF2HMAC(
@@ -18,11 +18,6 @@ kdf = PBKDF2HMAC(
 )
 key = base64.urlsafe_b64encode(kdf.derive(getpass().encode('utf-8')))
 fernet = Fernet(key)
-
-with AccessEncrypted("vault.csv", key) as file:
-    df = pd.read_csv(file)
-
-print(df)
 
 
 def encrypt_file(file):
@@ -42,3 +37,57 @@ def decrypt_file(file):
 
 
 # encrypt_file("vault.csv")
+# decrypt_file("vault.csv")
+
+
+def add_password(site, password):
+    with AccessEncrypted("vault.csv", key) as file:
+        df = pd.read_csv(file, index_col=0)
+        df.loc[site] = password
+        df.to_csv("vault.csv")
+    return password
+
+
+# print(add_password("google", "emmzybabez<3"))
+
+
+def retrieve_password(site):
+    with AccessEncrypted("vault.csv", key) as file:
+        df = pd.read_csv(file, index_col=0)
+        password = df.loc[site].password
+    return password
+
+
+# print(retrieve_password("google"))
+
+
+def delete_password(site):
+    with AccessEncrypted("vault.csv", key) as file:
+        df = pd.read_csv(file, index_col=0)
+        df = df.drop(site)
+        df.to_csv("vault.csv")
+    print(f"Password for {site} deleted.")
+
+
+# delete_password("twitter")
+
+
+def show_all():
+    with AccessEncrypted("vault.csv", key) as file:
+        df = pd.read_csv(file, index_col=0)
+        for index, row in df.iterrows():
+            print(index, row['password'])
+
+
+show_all()
+
+
+def purge_vault():
+    with AccessEncrypted("vault.csv", key) as file:
+        df = pd.read_csv(file, index_col=0)
+        df = df[0:0]
+        df.to_csv("vault.csv")
+    print("Vault deleted.")
+
+
+# purge_vault()
